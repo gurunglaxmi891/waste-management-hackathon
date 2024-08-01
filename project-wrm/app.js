314acca5aac53de04fbc52de28964c4c29a4f8b3
Subina Chhetri <120252788+subuchhetri34@users.noreply.github.com>
2024-08-01 15:11:17 +0545

    Schedule and admin

--- project-wrm/app.js
+++ project-wrm/app.js
@@ -8,6 +8,7 @@ var bcrypt = require("bcrypt");
 var indexRouter = require('./routes/index');
 
 var usersRouter = require('./routes/users');
+var adminRouter = require('/routes/admin');
 
 
 var app = express();
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users',usersRouter);
+app.use('admin/schedule',adminRouter);
 
 
 
