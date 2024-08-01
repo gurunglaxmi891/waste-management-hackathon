ab4e45a12883476f6bd786959d4face32ee41c22
Swostika Poudel <swostikapoudel@Bishals-Macbook.local>
2024-08-01 11:02:15 +0545

    add in index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// Middleware to check if user is admin
+const isAdmin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(403).send('Access denied');
+  }
+};
+
+// Admin panel route
+router.get('/admin', isAdmin, (req, res) => {
+  res.render('admin-panel', { adminName: req.user.name });
 });
 
 module.exports = router;
