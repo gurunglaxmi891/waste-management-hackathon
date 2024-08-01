ec0b7766978d267169c3af429f7153662316a650
Subina Chhetri <120252788+subuchhetri34@users.noreply.github.com>
2024-08-01 12:39:59 +0545

    Added login and signup page

--- project-wrm/app.js
+++ project-wrm/app.js
@@ -3,9 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bcrypt = require("bcrypt");
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
+var usersRouter = require('./routes/users')
+
 
 var app = express();
 
@@ -20,7 +23,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users',usersRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- project-wrm/routes/index.js
+++ project-wrm/routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'WP Notifier', subtitle: 'Waste Pickup Notifier'});
 });
+
+
 
 module.exports = router;
